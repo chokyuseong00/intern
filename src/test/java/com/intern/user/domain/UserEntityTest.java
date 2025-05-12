1daa7fb4d4116046125dc160d6034cd71ae9745b
Imnotcoderdude <topsolo7@naver.com>
2025-05-12 17:22:38 +0900

    test: user entity test - create user entity test code - of

---
+++ src/test/java/com/intern/user/domain/UserEntityTest.java
@@ -0,0 +1,27 @@
+package com.intern.user.domain;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.intern.user.domain.model.User;
+import com.intern.user.domain.model.UserRole;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@DisplayName("User 엔티티 테스트")
+public class UserEntityTest {
+
+    @Test
+    @DisplayName("사용자 엔티티 생성 테스트")
+    void userOfTest() {
+        String username = "testUser";
+        String password = "testPassword";
+
+        User user = User.of(username, password);
+
+        assertEquals(username, user.getUsername());
+        assertEquals(password, user.getPassword());
+        assertEquals(UserRole.USER, user.getRole());
+    }
+}
