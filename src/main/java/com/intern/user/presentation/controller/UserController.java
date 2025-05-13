765e6f95f2e77aed3ddc14994dac83d24a78a7f7
Imnotcoderdude <topsolo7@naver.com>
2025-05-13 17:29:37 +0900

    fix: 사용자 컨트롤러 추가 - 회원가입 엔드포인트 추가

---
+++ src/main/java/com/intern/user/presentation/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.intern.user.presentation.controller;
+
+import com.intern.user.application.dto.reqeust.UserSignupRequestDto;
+import com.intern.user.application.dto.response.UserSignupResponseDto;
+import com.intern.user.application.service.UserService;
+import java.net.URI;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<UserSignupResponseDto> signupUser(
+        @RequestBody UserSignupRequestDto requestDto
+    ) {
+        UserSignupResponseDto responseDto = userService.signupUser(requestDto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentContextPath()
+            .path("/api/v1/users/my-page")
+            .build()
+            .toUri();
+        return ResponseEntity.created(uri).body(responseDto);
+    }
+}
