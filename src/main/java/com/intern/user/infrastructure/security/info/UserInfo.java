3611ad91f71a9b8d9739ff4450d86ddf942d34e2
Imnotcoderdude <topsolo7@naver.com>
2025-05-13 23:12:49 +0900

    feat: 의존성 침범을 막기 위한 Security용 UserInfo Recode 클래스

---
+++ src/main/java/com/intern/user/infrastructure/security/info/UserInfo.java
@@ -0,0 +1,18 @@
+package com.intern.user.infrastructure.security.info;
+
+import io.jsonwebtoken.Claims;
+
+public record UserInfo(
+    String userId,
+    String username,
+    String role
+) {
+
+    public static UserInfo ofClaims(Claims claims) {
+        return new UserInfo(
+            claims.getSubject(),
+            claims.get("username", String.class),
+            claims.get("role", String.class)
+        );
+    }
+}
