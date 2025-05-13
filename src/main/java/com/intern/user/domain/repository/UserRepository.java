b328f14f374e737c82fe6e892828589790488dd8
Imnotcoderdude <topsolo7@naver.com>
2025-05-13 17:28:46 +0900

    feat: 사용자 회원가입시 DB 저장 및 검증 메서드 추가

--- src/main/java/com/intern/user/domain/repository/UserRepository.java
+++ src/main/java/com/intern/user/domain/repository/UserRepository.java
@@ -1,5 +1,10 @@
 package com.intern.user.domain.repository;
 
+import com.intern.user.domain.model.User;
+
 public interface UserRepository {
 
+    void save(User user);
+
+    boolean existsByUsername(String username);
 }
--- src/main/java/com/intern/user/infrastructure/repository/UserRepositoryImpl.java
+++ src/main/java/com/intern/user/infrastructure/repository/UserRepositoryImpl.java
@@ -1,5 +1,6 @@
 package com.intern.user.infrastructure.repository;
 
+import com.intern.user.domain.model.User;
 import com.intern.user.domain.repository.UserRepository;
 import com.intern.user.infrastructure.persistence.JpaUserRepository;
 import lombok.RequiredArgsConstructor;
@@ -11,4 +12,13 @@ public class UserRepositoryImpl implements UserRepository {
 
     private final JpaUserRepository jpaUserRepository;
 
+    @Override
+    public void save(User user) {
+        jpaUserRepository.save(user);
+    }
+
+    @Override
+    public boolean existsByUsername(String username) {
+        return jpaUserRepository.existsByUsername(username);
+    }
 }
