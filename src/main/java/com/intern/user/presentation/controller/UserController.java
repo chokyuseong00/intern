9ecc29e1b7afdb103953565e3ee25fde10294060
Imnotcoderdude <topsolo7@naver.com>
2025-05-13 21:58:39 +0900

    fix: 엔드포인트 요구사항에 맞춰 수정

--- src/main/java/com/intern/user/presentation/controller/UserController.java
+++ src/main/java/com/intern/user/presentation/controller/UserController.java
@@ -10,13 +10,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/users")
 public class UserController {
 
     private final UserService userService;
