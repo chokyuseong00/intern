16a20999f6c6e6d4f574f32e9ef7501621a3e82a
Imnotcoderdude <topsolo7@naver.com>
2025-05-13 17:29:09 +0900

    fix: 엔티티 사용자 권한 이넘 수정

--- src/main/java/com/intern/user/domain/model/User.java
+++ src/main/java/com/intern/user/domain/model/User.java
@@ -2,6 +2,8 @@ package com.intern.user.domain.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -30,6 +32,7 @@ public class User {
     private String nickname;
 
     @Column(name = "role", nullable = false)
+    @Enumerated(EnumType.STRING)
     private UserRole role;
 
     private User(
