c0534cc17d74928e22937d44c4a1d2565a07d669
Imnotcoderdude <topsolo7@naver.com>
2025-05-13 18:02:24 +0900

    feat: 로그인시 사용할 DTO 클래스 추가

---
+++ src/main/java/com/intern/user/application/dto/reqeust/UserLoginRequestDto.java
@@ -0,0 +1,11 @@
+package com.intern.user.application.dto.reqeust;
+
+import lombok.Getter;
+
+@Getter
+public class UserLoginRequestDto {
+
+    private String username;
+    private String password;
+
+}
