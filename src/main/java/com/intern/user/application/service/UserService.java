7b473380f3af615f7102a5f647194fb66379aa98
Imnotcoderdude <topsolo7@naver.com>
2025-05-13 23:15:37 +0900

    feat: 권한 변경 기능 서비스 코드 추가

--- src/main/java/com/intern/user/application/service/UserService.java
+++ src/main/java/com/intern/user/application/service/UserService.java
@@ -2,12 +2,16 @@ package com.intern.user.application.service;
 
 import com.intern.user.application.dto.reqeust.UserLoginRequestDto;
 import com.intern.user.application.dto.reqeust.UserSignupRequestDto;
+import com.intern.user.application.dto.response.AdminRoleUpdateResponseDto;
 import com.intern.user.application.dto.response.UserLoginResponseDto;
 import com.intern.user.application.dto.response.UserSignupResponseDto;
+import com.intern.user.domain.model.UserRole;
 
 public interface UserService {
 
     UserSignupResponseDto signupUser(UserSignupRequestDto requestDto);
 
     UserLoginResponseDto loginUser(UserLoginRequestDto requestDto);
+
+    AdminRoleUpdateResponseDto updateRole(Long userId, UserRole role);
 }
