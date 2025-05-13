846a46beb5cddb28691a646dafb405b5c08944aa
Imnotcoderdude <topsolo7@naver.com>
2025-05-13 18:01:05 +0900

    feat: 로그인시 에러 코드 추가

--- src/main/java/com/intern/user/global/exception/ErrorCode.java
+++ src/main/java/com/intern/user/global/exception/ErrorCode.java
@@ -8,7 +8,9 @@ public enum ErrorCode {
     USER_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 가입된 사용자입니다."),
     UNMATCHED_USER_DATA(HttpStatus.FORBIDDEN, "요청자의 정보가 아닙니다."),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
+    INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "아이디 또는 비밀번호가 올바르지 않습니다."),
     UNAUTHORIZED_ACCESS(HttpStatus.UNAUTHORIZED, "권한이 없습니다.");
+
 
     private final HttpStatus httpStatus;
 
