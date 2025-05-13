8f385e7d80ecb29479ac8788091fd188ee7319c4
Imnotcoderdude <topsolo7@naver.com>
2025-05-13 21:55:42 +0900

    feat: 사용자 로그인 기능 서비스 코드 추가

--- src/main/java/com/intern/user/application/service/UserService.java
+++ src/main/java/com/intern/user/application/service/UserService.java
@@ -1,9 +1,13 @@
 package com.intern.user.application.service;
 
+import com.intern.user.application.dto.reqeust.UserLoginRequestDto;
 import com.intern.user.application.dto.reqeust.UserSignupRequestDto;
+import com.intern.user.application.dto.response.UserLoginResponseDto;
 import com.intern.user.application.dto.response.UserSignupResponseDto;
 
 public interface UserService {
 
     UserSignupResponseDto signupUser(UserSignupRequestDto requestDto);
+
+    UserLoginResponseDto loginUser(UserLoginRequestDto requestDto);
 }
--- src/main/java/com/intern/user/application/service/UserServiceImpl.java
+++ src/main/java/com/intern/user/application/service/UserServiceImpl.java
@@ -1,21 +1,26 @@
 package com.intern.user.application.service;
 
+import com.intern.user.application.dto.reqeust.UserLoginRequestDto;
 import com.intern.user.application.dto.reqeust.UserSignupRequestDto;
+import com.intern.user.application.dto.response.UserLoginResponseDto;
 import com.intern.user.application.dto.response.UserSignupResponseDto;
 import com.intern.user.application.mapper.UserMapper;
 import com.intern.user.domain.model.User;
 import com.intern.user.domain.repository.UserRepository;
 import com.intern.user.domain.validator.UserValidator;
+import com.intern.user.infrastructure.password.PasswordUtil;
+import com.intern.user.infrastructure.security.jwt.JwtService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final UserValidator userValidator;
+    private final JwtService jwtService;
 
     @Override
     @Transactional
@@ -24,5 +29,14 @@ public class UserServiceImpl implements UserService{
         User user = UserMapper.toUser(requestDto);
         userRepository.save(user);
         return UserMapper.toSignupResDto(user);
+    }
+
+    @Override
+    public UserLoginResponseDto loginUser(UserLoginRequestDto requestDto) {
+        User user = userRepository.findByUsername(requestDto.getUsername());
+        PasswordUtil.isMatched(requestDto.getPassword(), user.getPassword());
+
+        String accessToken = jwtService.createAccessToken(user);
+        return UserMapper.toLoginResDto(accessToken);
     }
 }
