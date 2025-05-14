220f4c6f7ccbac69408b81ffdc758e58121c279e
Imnotcoderdude <topsolo7@naver.com>
2025-05-14 19:25:53 +0900

    feat: UserInfo 레코드에 토큰 만료시간 필드 추가

--- src/main/java/com/intern/user/security/info/UserInfo.java
+++ src/main/java/com/intern/user/security/info/UserInfo.java
@@ -1,18 +1,21 @@
 package com.intern.user.security.info;
 
 import io.jsonwebtoken.Claims;
+import java.time.Instant;
 
 public record UserInfo(
     String userId,
     String username,
-    String role
+    String role,
+    Instant expTime
 ) {
 
     public static UserInfo ofClaims(Claims claims) {
         return new UserInfo(
             claims.getSubject(),
             claims.get("username", String.class),
-            claims.get("role", String.class)
+            claims.get("role", String.class),
+            claims.getExpiration().toInstant()
         );
     }
 }
