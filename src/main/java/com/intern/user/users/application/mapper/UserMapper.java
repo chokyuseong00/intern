0c1c65453be0c5cd21619b6f7dfc238d9ff0428d
Imnotcoderdude <topsolo7@naver.com>
2025-05-14 22:18:00 +0900

    fix: 권한 변경 응답값 mapper 수정

--- src/main/java/com/intern/user/users/application/mapper/UserMapper.java
+++ src/main/java/com/intern/user/users/application/mapper/UserMapper.java
@@ -25,7 +25,7 @@ public class UserMapper {
     }
 
     public static AdminRoleUpdateResponseDto toUpdateRoleResDto(User user) {
-        return AdminRoleUpdateResponseDto.of(user.getUsername(), user.getPassword(),
+        return AdminRoleUpdateResponseDto.of(user.getUsername(), user.getNickname(),
             user.getRole());
     }
 
