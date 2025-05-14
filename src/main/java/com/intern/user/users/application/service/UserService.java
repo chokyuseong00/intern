886d235e6fef34721ed3f0afec9f208abda5b40b
Imnotcoderdude <topsolo7@naver.com>
2025-05-14 22:18:27 +0900

    fix: 권한 검증 책임 이전으로 인한 서비스 파라미터 수정

--- src/main/java/com/intern/user/users/application/service/UserService.java
+++ src/main/java/com/intern/user/users/application/service/UserService.java
@@ -7,7 +7,6 @@ import com.intern.user.users.application.dto.response.AdminRoleUpdateResponseDto
 import com.intern.user.users.application.dto.response.AdminSignupResponseDto;
 import com.intern.user.users.application.dto.response.UserLoginResponseDto;
 import com.intern.user.users.application.dto.response.UserSignupResponseDto;
-import com.intern.user.users.domain.model.UserRole;
 
 public interface UserService {
 
@@ -15,7 +14,7 @@ public interface UserService {
 
     UserLoginResponseDto loginUser(UserLoginRequestDto requestDto);
 
-    AdminRoleUpdateResponseDto updateRole(Long userId, UserRole role);
+    AdminRoleUpdateResponseDto updateRole(Long userId);
 
     AdminSignupResponseDto signupAdmin(AdminSignupRequestDto requestDto);
 }
